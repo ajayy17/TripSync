949835b86133b3018faab2bb38d38689218742d9
ajayyy17 <ajayyadav6511@gmail.com>
2025-06-17 02:56:22 +0530

    First frontend commit by Ajay

---
+++ FrontEnd/tripsync-app/src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <h3>Enter your Email</h3>
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
