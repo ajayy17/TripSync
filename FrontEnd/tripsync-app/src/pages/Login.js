028ea3ada74480c3efddde6163ee5701760c2115
ajayyy17 <ajayyadav6511@gmail.com>
2025-06-21 03:42:32 +0530

    Login and Register call successfull Ajay

--- FrontEnd/tripsync-app/src/pages/Login.js
+++ FrontEnd/tripsync-app/src/pages/Login.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import "../styles/Login.css";
+import Register from "./Register";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
   const updateEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -12,28 +18,67 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const SignIn = () => {
-    //Call the api and pass the data
+  const LogIn = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:9090/tripSync/api/auth/signin",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            username: email,
+            password: password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Login Failed");
+      }
+
+      localStorage.setItem("token", data.jwtToken);
+      setMessage("Successfully Logged In");
+    } catch (error) {
+      setMessage(error.message);
+    }
+    console.log(message);
+  };
+  const SignUp = () => {
+    navigate("/register");
   };
   return (
     <>
-      <div>
+      <div className="loginHeader">
         <div>
           <h1>Login</h1>
           <div>
-            <h3>Enter your Email</h3>
-            <input type="text" onChange={updateEmail} />
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={updateEmail}
+            />
           </div>
         </div>
         <div>
-          <h1>Password</h1>
           <div>
-            <h3>Enter your password</h3>
-            <input type="text" onChange={updatePassword} />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={updatePassword}
+            />
           </div>
         </div>
         <div>
-          <button onClick={SignIn}>SingIn</button>
+          <button onClick={LogIn}>Login</button>
+        </div>
+        <div>
+          <button onClick={SignUp}>SignUp</button>
         </div>
       </div>
     </>
